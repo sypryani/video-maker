2e0844a40264d1e400ed42f10ed70db2e7107524
Ulisses Luiz Sypryani - DATAPREVDF <ulisses.sypryani@BSAD275628.GOV>
2019-03-21 11:27:08 -0300

    feat: add search term and prefix

---
+++ index.js
@@ -0,0 +1,25 @@
+const readline = require('readline-sync')
+
+function start() {
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
+    function askAndReturnSearchTerm() {
+        return readline.question('Type a wikipedia search term: ')
+    }
+
+    function askAndReturnPrefix() {
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ') // captura opção do usuário pelo teclado
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+        // console.log(selectedPrefixText)
+        return selectedPrefixText
+    }
+
+    console.log(content)
+}
+
+start()
